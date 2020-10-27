98ed6ef1ef13d399c4c53cdb929b68aff97442a4
Grzegorz-Giedrojc <grzesiekodkrywaja@gmail.com>
2020-10-27 16:27:06 +0100

    Templates

--- encyklopedia_app/urls.py
+++ encyklopedia_app/urls.py
@@ -1,6 +1,13 @@
+from django.conf.urls import url, include
+from .import views
 from django.urls import path
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
