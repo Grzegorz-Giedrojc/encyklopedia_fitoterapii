e66650dc6ebf0aaff6dcbbcb93e1e4b0f75533da
Grzegorz-Giedrojc <grzesiekodkrywaja@gmail.com>
2020-11-17 17:27:49 +0100

    xxxxxx

--- encyklopedia_app/views.py
+++ encyklopedia_app/views.py
@@ -17,3 +17,12 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'encyklopedia_app/post_detail.html', {'post': post})
+
+def get_blog_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        posts = Post.objects.filter(
+            Q(title__icontrains=q) |
+            Q(body__icontrains=q)
+        ).distinc
