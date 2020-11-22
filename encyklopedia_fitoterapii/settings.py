c2cf6e0053c1650b8a863d5c1b8ca948e939c9ce
Grzegorz-Giedrojc <grzesiekodkrywaja@gmail.com>
2020-11-22 18:22:41 +0100

    css

--- encyklopedia_fitoterapii/settings.py
+++ encyklopedia_fitoterapii/settings.py
@@ -133,3 +133,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_REDIRECT_URL = "/domowa_apteczka/"
+LOGOUT_REDIRECT_URL = "/"
--- encyklopedia_fitoterapii/urls.py
+++ encyklopedia_fitoterapii/urls.py
@@ -25,6 +25,7 @@ urlpatterns=[
     path('admin/', admin.site.urls),
     path('', include('encyklopedia_app.urls')),
     path('register/', v.register, name="register"),
+    path('', include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
