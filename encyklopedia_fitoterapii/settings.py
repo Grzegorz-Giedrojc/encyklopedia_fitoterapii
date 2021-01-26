1ffc861df087d31e04f421d8a51a5845442fa9cc
Grzegorz-Giedrojc <grzesiekodkrywaja@gmail.com>
2021-01-26 12:23:40 +0100

    na pythonanywhere

--- encyklopedia_fitoterapii/settings.py
+++ encyklopedia_fitoterapii/settings.py
@@ -130,8 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-CRISPY_TEMPLATE_PACK="bootstrap4"
-
 LOGIN_REDIRECT_URL = "/domowa_apteczka/"
 LOGOUT_REDIRECT_URL = "/"
 
